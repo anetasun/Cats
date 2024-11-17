a206b6dcb9ce7fe6df330e5d65fee00d50b31c48
anetasun <anetsn@gmail.com>
2024-11-17 15:19:28 +0300

    Вместо кнопки добавлено меню

--- Cataas.py
+++ Cataas.py
@@ -34,6 +34,9 @@ def set_image():
         label.image = img
 
 
+def exit():
+    window.destroy()
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -42,8 +45,20 @@ label = Label()
 label.pack()
 
 # Добавляем кнопку для обновления изображения
-update_button = Button(text="Обновить", command=set_image)
-update_button.pack()
+#update_button = Button(text="Обновить", command=set_image)
+#update_button.pack()
+
+# Создаем меню
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+# Добавляем пункты меню
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
+
 
 
 url = "https://cataas.com/cat"
